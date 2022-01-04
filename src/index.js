58035b62e803917d4ac55634df0515a861858293
nascott <nascott94@outlook.com>
2022-01-04 09:52:34 -0700

    restarting the assets

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import GlobalStyles from "./styles/GlobalStyles";
-import Typography from "./styles/Typography";
 
 import App from "./App";
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <Typography />
     <App />
   </>,
   document.getElementById("root")
