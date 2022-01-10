a8cf3585d8d8f6aa2a8a60f074f81986fc95dfb6
nascott <nascott94@outlook.com>
2022-01-10 12:51:40 -0700

    projectsection component

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -13,8 +13,60 @@ const AboutSectionStyles = styled.div`
     justify-content: flex-start;
     text-align: left;
   }
-  .aboutSection__left .aboutSection__right {
+  .aboutSection__left,
+  .aboutSection__right {
     flex: 1;
+  }
+  .section-title {
+    text-align: left;
+  }
+  .para {
+    margin-top: 2rem;
+    margin-left: 0;
+  }
+  .aboutSection__buttons {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 2rem;
+    margin-top: 2rem;
+  }
+  @media only screen and (max-width: 950px) {
+    .aboutSection__left {
+      flex: 4;
+    }
+    .aboutSection__right {
+      flex: 3;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      text-align: center;
+    }
+    .aboutSection__left,
+    .aboutSection__right {
+      width: 100%;
+    }
+    .aboutSection__right {
+      margin-top: 3rem;
+    }
+    .section-title {
+      text-align: center;
+    }
+    .para {
+      margin: 0 auto;
+      margin-top: 2rem;
+    }
+    .aboutSection__buttons {
+      flex-direction: column;
+      gap: 0rem;
+      .button-wrapper,
+      a {
+        width: 100%;
+        text-align: center;
+      }
+    }
   }
 `;
 
@@ -37,6 +89,9 @@ export default function AboutSection() {
             <Button btnText="Works" btnLink="/projects" />
             <Button btnText="Read More" btnLink="/about" />
           </div>
+        </div>
+        <div className="aboutSection__right">
+          <img className="aboutImg" src={AboutImg} alt="img" />
         </div>
       </div>
     </AboutSectionStyles>
