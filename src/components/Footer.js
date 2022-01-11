3ffcaa72bc450012007668f7fe4a02ef81c3995c
nascott <nascott94@outlook.com>
2022-01-11 10:25:57 -0700

    footer component created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
 import PText from './PText';
+import FooterCol from './FooterCol';
+
+const FooterStyle = styled.div``;
 
 export default function Footer() {
-  return <div>joji</div>;
+  return (
+    <FooterStyle>
+      <div className="container">
+        <div className="footer__col1">
+          <div className="footer__col1__title">Natalie Scott</div>
+          <PText>joji joji joji</PText>
+        </div>
+        <div className="footer__col2">
+          <FooterCol
+            heading="Important Links"
+            links={[
+              {
+                title: 'Home',
+                path: '/',
+                type: 'Link',
+              },
+              {
+                type: 'Link',
+                title: 'About',
+                path: '/about',
+              },
+              {
+                type: 'Link',
+                title: 'Projects',
+                path: '/projects',
+              },
+              {
+                type: 'Link',
+                title: 'Contact',
+                path: '/contact',
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </FooterStyle>
+  );
 }
---
+++ src/components/FooterCol.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const ColStyle = styled.div``;
+
+export default function FooterCol({
+  heading = 'Col Heading',
+  links = [
+    { type: 'Link', title: 'Home', path: '/Home' },
+    { type: 'Link', title: 'About', path: '/about' },
+  ],
+}) {
+  return (
+    <ColStyle>
+      <h2 className="heading">{heading}</h2>
+      <ul>
+        {links.map((item, index) => (
+          <li key={index}>
+            {item.type === 'Link' ? (
+              <Link to={item.path}>{item.title}</Link>
+            ) : (
+              <a href={item.path} target="_blank" rel="noreferrer">
+                {item.title}
+              </a>
+            )}
+          </li>
+        ))}
+      </ul>
+    </ColStyle>
+  );
+}
