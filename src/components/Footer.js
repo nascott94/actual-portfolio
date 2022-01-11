d2257fb4945f656431ebd6625796861839dd11d3
nascott <nascott94@outlook.com>
2022-01-11 11:05:52 -0700

    creating parts of the project page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,38 @@ import styled from 'styled-components';
 import PText from './PText';
 import FooterCol from './FooterCol';
 
-const FooterStyle = styled.div``;
+const FooterStyle = styled.div`
+  background-color: var(--deep-dark);
+  padding-top: 10rem;
+  .container {
+    display: flex;
+    gap: 3rem;
+  }
+  .footer-col1 {
+    flex: 2;
+  }
+  .footer__col2,
+  .footer__col3,
+  .footer__col4 {
+    flex: 1;
+  }
+  .footer__col1__title {
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      gap: 0rem;
+      & > div {
+        margin-top: 5rem;
+      }
+    }
+    .footer__col1 .para {
+      max-width: 100%;
+    }
+  }
+`;
 
 export default function Footer() {
   return (
@@ -36,6 +67,44 @@ export default function Footer() {
                 type: 'Link',
                 title: 'Contact',
                 path: '/contact',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col3">
+          <FooterCol
+            heading="Contact Info"
+            links={[
+              {
+                title: '386-506-1792',
+                path: 'tel:+386-506-1792',
+              },
+              {
+                title: 'nascott94@outlook.com',
+                path: 'mailto:nascott94@outlook.com',
+              },
+              {
+                title: 'Denver, CO',
+                path: '',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col4">
+          <FooterCol
+            heading="social Links"
+            links={[
+              {
+                title: 'Facebook',
+                path: 'http://facebook.com',
+              },
+              {
+                title: 'Twitter',
+                path: 'http://twitter.com',
+              },
+              {
+                title: 'Instagram',
+                path: 'http://instagram.com',
               },
             ]}
           />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { MdSearch } from 'react-icons/md';
+import SectionTitle from '../components/SectionTitle';
+import ProjectInfo from '../assets/data/projects';
+import ProjectItem from '../components/ProjectItem';
+
+const ProjectStyle = styled.div`
+  padding: 10rem 0;
+`;
 
 export default function Projects() {
+  const [searchText, setSearchText] = useState('');
+  const [projectsData, setProjectsData] = useState(ProjectInfo);
+  useEffect(() => {
+    if (searchText === '') return;
+    setProjectsData(() =>
+      ProjectInfo.filter((item) =>
+        item.name.toLowerCase().match(searchText.toLowerCase())
+      )
+    );
+  }, [searchText]);
+
   return (
-    <div>
+    <ProjectStyle>
       <h1>this is projects page</h1>
-    </div>
+    </ProjectStyle>
   );
 }
